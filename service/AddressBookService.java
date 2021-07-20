1d25a0c3df19c63de2354be6677cb8b61e1d079f
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-07-21 00:20:15 +0530

    Add: Interface & Abstract methods

--- service/AddressBookService.java
+++ service/AddressBookService.java
@@ -1,137 +1,115 @@
 package com.addressbook.service;
 
-import java.util.List;
 import com.addressbook.dao.AddressBookDAO;
 import com.addressbook.model.ContactDetails;
 
+import java.util.List;
+
 public class AddressBookService implements AddressBookServiceInterface {
+    AddressBookDAO addressDAO = new AddressBookDAO();
 
-	AddressBookDAO addressDAO = new AddressBookDAO();
-	
-	@Override
-	public boolean isAddressBookPresent(String addrName) {
-		
-		return addressDAO.isAddressBookPresent(addrName);
-	}
-	
-	@Override
-	public void addAddressBook(String addrName, List<ContactDetails> contactList) {
-		
-		addressDAO.addAddressBook(addrName, contactList);
-	}
+    @Override
+    public boolean isAddressBookPresent(String addrName) {
+        return addressDAO.isAddressBookPresent(addrName);
+    }
 
-	@Override
-	public boolean isContactPresent(String addrName, String contactName) {
-		
-		return addressDAO.isContactPresent(addrName, contactName);
-	}
-	
-	@Override
-	public List<ContactDetails> getExistingContactList(String addrName) {
-		
-		return addressDAO.getExistingContactList(addrName);
-	}
+    @Override
+    public void addAddressBook(String addrName, List<ContactDetails> contactList) {
+        addressDAO.addAddressBook(addrName, contactList);
+    }
 
-	@Override
-	public void editContactDetails(String addrName, String contactName, ContactDetails contactDetails) {
-		
-		addressDAO.editContactDetails(addrName, contactName, contactDetails);
-	}
-	
-	@Override
-	public void deleteAddressBook(String addrName) {
-		
-		addressDAO.deleteAddressBook(addrName);
-	}
-	
-	@Override
-	public void deleteContact(String addrName, String contactName) {
-		
-		addressDAO.deleteContact(addrName, contactName);
-	}
-	
-	@Override
-	public void viewAddressBook() {
-		
-		addressDAO.viewAddressBook();
-	}
+    @Override
+    public boolean isContactPresent(String addrName, String contactName) {
+        return addressDAO.isContactPresent(addrName, contactName);
+    }
 
-	@Override
-	public void viewSpecificAddressBook(String addrName) {
-		
-		addressDAO.viewSpecificAddressBook(addrName);
-	}
+    @Override
+    public List<ContactDetails> getExistingContactList(String addrName) {
+        return addressDAO.getExistingContactList(addrName);
+    }
 
-	@Override
-	public boolean checkCityPresent(String cityName) {
-		
-		return addressDAO.checkCityPresent(cityName);
-	}
+    @Override
+    public void editContactDetails(String addrName, String contactName, ContactDetails contactDetails) {
+        addressDAO.editContactDetails(addrName, contactName, contactDetails);
+    }
 
-	@Override
-	public boolean checkStatePresent(String stateName) {
-		
-		return addressDAO.checkStatePresent(stateName);
-	}
-	
-	@Override
-	public void checkContactInCity(String cityName, String contactName) {
-		
-		addressDAO.checkContactInCity(cityName, contactName);
-	}
+    @Override
+    public void deleteAddressBook(String addrName) {
+        addressDAO.deleteAddressBook(addrName);
+    }
 
-	@Override
-	public void checkContactInState(String stateName, String contactName) {
-		
-		addressDAO.checkContactInState(stateName, contactName);
-	}
+    @Override
+    public void deleteContact(String addrName, String contactName) {
+        addressDAO.deleteContact(addrName, contactName);
+    }
 
-	@Override
-	public void viewContactsByCityName(String cityName) {
-		
-		addressDAO.viewContactsByCityName(cityName);
-	}
+    @Override
+    public void viewAddressBook() {
+        addressDAO.viewAddressBook();
+    }
 
-	@Override
-	public void viewContactsByStateName(String stateName) {
-		
-		addressDAO.viewContactsByStateName(stateName);
-	}
+    @Override
+    public void viewSpecificAddressBook(String addrName) {
+        addressDAO.viewSpecificAddressBook(addrName);
+    }
 
-	@Override
-	public void getContactCountByCity(String cityName) {
-		
-		addressDAO.getContactCountByCity(cityName);
-	}
+    @Override
+    public boolean checkCityPresent(String cityName) {
+        return addressDAO.checkCityPresent(cityName);
+    }
 
-	@Override
-	public void getContactCountByState(String stateName) {
-		
-		addressDAO.getContactCountByState(stateName);
-	}
+    @Override
+    public boolean checkStatePresent(String stateName) {
+        return addressDAO.checkStatePresent(stateName);
+    }
 
-	@Override
-	public void viewInSortedByName(String addrName) {
-		
-		addressDAO.viewInSortedByName(addrName);
-	}
+    @Override
+    public void checkContactInCity(String cityName, String contactName) {
+        addressDAO.checkContactInCity(cityName, contactName);
+    }
 
-	@Override
-	public void viewInSortedByCity(String addrName) {
-		
-		addressDAO.viewInSortedByCity(addrName);
-	}
+    @Override
+    public void checkContactInState(String stateName, String contactName) {
+        addressDAO.checkContactInState(stateName, contactName);
+    }
 
-	@Override
-	public void viewInSortedByState(String addrName) {
-		
-		addressDAO.viewInSortedByState(addrName);
-	}
+    @Override
+    public void viewContactsByCityName(String cityName) {
+        addressDAO.viewContactsByCityName(cityName);
+    }
 
-	@Override
-	public void viewInSortedByZip(String addrName) {
-		
-		addressDAO.viewInSortedByZip(addrName);
-	}
-	
+    @Override
+    public void viewContactsByStateName(String stateName) {
+        addressDAO.viewContactsByStateName(stateName);
+    }
+
+    @Override
+    public void getContactCountByCity(String cityName) {
+        addressDAO.getContactCountByCity(cityName);
+    }
+
+    @Override
+    public void getContactCountByState(String stateName) {
+        addressDAO.getContactCountByState(stateName);
+    }
+
+    @Override
+    public void viewInSortedByName(String addrName) {
+        addressDAO.viewInSortedByName(addrName);
+    }
+
+    @Override
+    public void viewInSortedByCity(String addrName) {
+        addressDAO.viewInSortedByCity(addrName);
+    }
+
+    @Override
+    public void viewInSortedByState(String addrName) {
+        addressDAO.viewInSortedByState(addrName);
+    }
+
+    @Override
+    public void viewInSortedByZip(String addrName) {
+        addressDAO.viewInSortedByZip(addrName);
+    }
 }
--- service/AddressBookServiceInterface.java
+++ service/AddressBookServiceInterface.java
@@ -1,56 +1,53 @@
 package com.addressbook.service;
 
-import java.util.List;
 import com.addressbook.model.ContactDetails;
+import java.util.List;
 /**
- * AddressBookServiceInterface	--	Defining Abstract methods of all operations 
+ * AddressBookServiceInterface	--	Defining Abstract methods of all operations
  * 									performed in AddressBook
- * 
- * @author Abhishek Shigavan
  *
+ * @author Abhishek Shigavan
  */
 public interface AddressBookServiceInterface {
-	
-	public abstract boolean isAddressBookPresent(String addrName);
-	
-	public abstract void addAddressBook(String addrName, List<ContactDetails> contactList);
-	
-	public abstract boolean isContactPresent(String addrName, String contactName);
-	
-	public abstract List<ContactDetails> getExistingContactList(String addrName);
-	
-	public abstract void editContactDetails(String addrName, String contactName, ContactDetails contactDetails);
-	
-	public abstract void deleteAddressBook(String addrName);
-	
-	public abstract void deleteContact(String addrName, String contactName);
-	
-	public abstract void viewAddressBook();
-	
-	public abstract void viewSpecificAddressBook(String addrName);
+    public abstract boolean isAddressBookPresent(String addrName);
 
-	public abstract boolean checkCityPresent(String cityName);
+    public abstract void addAddressBook(String addrName, List<ContactDetails> contactList);
 
-	public abstract boolean checkStatePresent(String stateName);
-	
-	public abstract void checkContactInCity(String cityName, String contactName);
-	
-	public abstract void checkContactInState(String stateName, String contactName);
-	
-	public abstract void viewContactsByCityName(String cityName);
+    public abstract boolean isContactPresent(String addrName, String contactName);
 
-	public abstract void viewContactsByStateName(String stateName);
+    public abstract List<ContactDetails> getExistingContactList(String addrName);
 
-	public abstract void getContactCountByCity(String cityName);
+    public abstract void editContactDetails(String addrName, String contactName, ContactDetails contactDetails);
 
-	public abstract void getContactCountByState(String stateName);
+    public abstract void deleteAddressBook(String addrName);
 
-	public abstract void viewInSortedByName(String addrName);
+    public abstract void deleteContact(String addrName, String contactName);
 
-	public abstract void viewInSortedByCity(String addrName);
+    public abstract void viewAddressBook();
 
-	public abstract void viewInSortedByState(String addrName);
+    public abstract void viewSpecificAddressBook(String addrName);
 
-	public abstract void viewInSortedByZip(String addrName);
-	
+    public abstract boolean checkCityPresent(String cityName);
+
+    public abstract boolean checkStatePresent(String stateName);
+
+    public abstract void checkContactInCity(String cityName, String contactName);
+
+    public abstract void checkContactInState(String stateName, String contactName);
+
+    public abstract void viewContactsByCityName(String cityName);
+
+    public abstract void viewContactsByStateName(String stateName);
+
+    public abstract void getContactCountByCity(String cityName);
+
+    public abstract void getContactCountByState(String stateName);
+
+    public abstract void viewInSortedByName(String addrName);
+
+    public abstract void viewInSortedByCity(String addrName);
+
+    public abstract void viewInSortedByState(String addrName);
+
+    public abstract void viewInSortedByZip(String addrName);
 }
